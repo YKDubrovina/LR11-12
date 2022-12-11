f88888f6217acf6059520df1a5653b34aa2bd9f1
YKDubrovina <117807857+YKDubrovina@users.noreply.github.com>
2022-12-11 23:02:08 +0300

    Add files via upload

---
+++ 1.0(1).pas
@@ -0,0 +1,11 @@
+﻿uses GraphABC;
+begin
+  SetWindowsize(600, 400);
+  SetPenWidth(5);
+  SetPenColor(clGreen);
+  MoveTo (200, 100);
+  LineTo (300,100);
+  lineTo (300, 200);
+  LineTo (200, 200);
+  LineTo (200, 100);
+end.
---
+++ 1.0(2).pas
@@ -0,0 +1,10 @@
+﻿uses GraphABC;
+begin 
+  SetWindowSize(600, 400);
+  SetPenColor(clRed);
+  SetPenWidth(5);
+  MoveTo (300, 100);
+  LineTo (400, 200);
+  LineTo(200, 200);
+  LineTo(300, 100);
+end.
